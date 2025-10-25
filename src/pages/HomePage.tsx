f4352ca6914040aef4ffc62e0a2e06ba91ba65a3
Yafei Deng <weihaianni@gmail.com>
2025-10-26 09:18:39 +1100

    fixed usenavigate issue"

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,29 +1,35 @@
-import { useAuth } from '../contexts/AuthContext'
+import { useEffect } from 'react';
+import { useAuth } from '../contexts/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import Footer from '../components/Layout/Footer';
 
 export default function HomePage() {
   const { user } = useAuth();
   const navigate = useNavigate();
-  if (!user) {
-    
-    navigate('/login');
-    return null;
-  } else {
-    return (
-      <div>
-        <h1>Welcome, {user.displayName} start your order now!</h1>
-        <button onClick={() => navigate('/menu')}>Start</button>
-        <footer
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/login');
+    }
+  }, [user, navigate]);
+
+  if (!user) return null; 
+
+  return (
+    <div>
+      <h1>Welcome, {user.displayName} start your order now!</h1>
+      <button onClick={() => navigate('/menu')}>Start</button>
+      <footer
         style={{
           marginLeft: "10%",
           height:"50px",
           width:"90%",
           border:"1px solid blue",
           marginBottom:"5px"
-        }}><Footer/></footer>
-      </div>
-      
-    )
-  }
+        }}
+      >
+        <Footer />
+      </footer>
+    </div>
+  );
 }
