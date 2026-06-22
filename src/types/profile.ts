e08c496c605053af05418c5227c6320fe5ff4cef
felix <weihaianni@gmail.com>
2026-06-22 16:37:01 +1000

    Add costomer service to add and get customers from firebase

--- src/types/profile.ts
+++ src/types/profile.ts
@@ -3,9 +3,12 @@ import { orderItemSchema, zTimestamp } from "./order"
 
 export const CustomerProfile = z.object(
     {
+        id: z.string().optional,
         customerName: z.string().min(3),
         options: z.array(orderItemSchema),
         createdAt: zTimestamp,
         updatedAt: zTimestamp
     }
 )
+
+export type Customer = z.infer<typeof CustomerProfile>
